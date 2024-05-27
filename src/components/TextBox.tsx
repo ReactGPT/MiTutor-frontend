77070a81b3653aaad183e867c58a6d46f003bebf
Diego Ochoa <daochoa@pucp.edu.pe>
2024-05-26 22:42:44 -0500

    detalleProgramabyAlumno layout fix

--- src/components/TextBox.tsx
+++ src/components/TextBox.tsx
@@ -1,24 +1,22 @@
 type textBoxProps = {
-
-    nombre: string;
-    contenido: string;
-
+  nombre: string;
+  contenido: string;
+  className?: string;
 };
 
-const TextBox = ({nombre,contenido}: textBoxProps) => {
-
-    return(
-        <div>
-            <div >
-                <span className="font-roboto text-2xl text-primary">
-                    {nombre}
-                </span>
-            </div>
-            <div className="w-full h-1/2 p-2 border-custom shadow-custom bg-[rgba(237,_238,_250,_0.50)] font-roboto text-terciary">
-                {contenido}
-            </div>
-        </div>
-    );
+const TextBox = ({ nombre, contenido, className }: textBoxProps) => {
+  return (
+    <div className={className}>
+      <div >
+        <span className="font-roboto text-lg text-primary">
+          {nombre}
+        </span>
+      </div>
+      <div className="text-base w-full h-1/2 p-1 px-3 border-custom shadow-custom bg-[rgba(237,_238,_250,_0.50)] font-roboto text-terciary">
+        {contenido}
+      </div>
+    </div>
+  );
 
 };
 

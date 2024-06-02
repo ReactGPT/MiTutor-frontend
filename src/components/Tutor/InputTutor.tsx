757df89eb9916669bfd251af78dc1e3ccc9ef297
Diego Ochoa <daochoa@pucp.edu.pe>
2024-06-01 19:44:35 -0500

    minor fix

--- src/components/Tutor/InputTutor.tsx
+++ src/components/Tutor/InputTutor.tsx
@@ -9,16 +9,16 @@ type InputProps = {
   readOnly?: boolean;
   onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
   manejarBlur?: (e: ChangeEvent<HTMLInputElement>) => void;
-}
+};
 
-const InputTutor = ({ titulo = "", texto = "", enable = true, name, value, onChange,readOnly,manejarBlur }: InputProps) => {
+const InputTutor = ({ titulo = "", texto = "", enable = true, name, value, onChange, readOnly, manejarBlur }: InputProps) => {
   return (
-    <li className="drop-shadow-md p-5 flex items-center  space-x-5 height-100% personalizado"
+    <li className="drop-shadow-md p-3 flex items-center space-x-5 height-100% personalizado"
       style={{ display: "flex", height: "100%", flexDirection: "column", alignItems: "flex-start" }}>
-      <label htmlFor="">{titulo}</label>
+      <label className="font-roboto text-base text-primary" htmlFor="">{titulo}</label>
       <input type="text"
-        className={`w-full px-3 py-2 mt-1 font-montserrat text-[90%] bg-[rgba(255,_255,_255,_0.50)] border-custom drop-shadow-md font-bold ${readOnly ? 'text-gray-500' : ''}`}
-        style={{ height: "100%", marginLeft: "0" }}
+        className={`w-full h-full px-3 py-2 mt-1 font-roboto text-base bg-[rgba(255,_255,_255,_0.50)] border-custom drop-shadow-md font-normal ${readOnly ? 'text-gray-500' : ''}`}
+        style={{ marginLeft: "0" }}
         placeholder={texto}
         disabled={!enable}
         name={name}
--- src/pages/tutor/miPerfil/PageMiPerfilTutor.tsx
+++ src/pages/tutor/miPerfil/PageMiPerfilTutor.tsx
@@ -1,32 +1,32 @@
 import InputTutor from '../../../components/Tutor/InputTutor';
-//import image from '/src/assets/Tutor/no-avatar.webp';
+import image from '/src/assets/Tutor/no-avatar.webp';
 import { Label } from 'flowbite-react';
 import { useAuth } from '../../../context';
 
 const PageMiPerfilTutor: React.FC = () => {
   const { userData } = useAuth();
   return (
-    <div className="w-full h-full flex">
-      <div className="w-1/2 flex flex-col">
-        <div className="flex justify-center">
-          <h1 className="font-montserrat text-[50px] font-bold text-primary pt-12">{`${userData?.userInfo?.personInfo.name} ${userData?.userInfo?.personInfo.lastName} ${userData?.userInfo?.personInfo.secondLastName}`}</h1>
-        </div>
-        <div className="flex-1 pt-12">
-          <ul className="px-11">
-            <Label value="Codigo:" className="text-primary font-roboto" />
-            <InputTutor texto="20004587" enable={false} />
-            <Label value="Correo Electronico:" className="text-primary font-roboto" />
-            <InputTutor texto={userData?.email} enable={false} />
-            <Label value="Telefono:" className="text-primary font-roboto" />
-            <InputTutor texto="998675729" enable={false} />
-          </ul>
+    <div className="flex w-full h-full justify-center items-center">
+
+      <div className="flex w-2/3 h-full justify-center items-center">
+        <div className="flex-col w-2/3 justify-center items-center gap-6">
+          <h1 className="font-montserrat text-4xl font-bold text-primary">{`${userData?.userInfo?.personInfo.name} ${userData?.userInfo?.personInfo.lastName} ${userData?.userInfo?.personInfo.secondLastName}`}</h1>
+          <InputTutor titulo="Código" texto="20004587" enable={false} />
+          <InputTutor titulo="Correo Electrónico" texto={userData?.email} enable={false} />
+          <InputTutor titulo="Telefono" texto="998675729" enable={false} />
         </div>
       </div>
-      <div className="w-1/2">
-        <div className="flex w-full justify-center items-center py-48">
-          <img src={userData?.imageUrl} alt="Imagen Tutor" className="w-[200px] h-[200px] rounded-full" />
+
+      <div className="flex w-1/3 h-full justify-center items-center">
+        <div className="flex w-full">
+          {userData ?
+            <img src={userData?.imageUrl} alt="Imagen Tutor" className="w-[200px] h-[200px] rounded-full" />
+            :
+            <img src={image} alt="Imagen Tutor" className="w-[200px] h-[200px] rounded-full" />
+          }
         </div>
       </div>
+
     </div>
   );
 };
